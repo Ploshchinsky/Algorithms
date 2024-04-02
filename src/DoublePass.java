57b36cebf5e1ca49b7c6ece26018dce046cc7d65
Anton P. <made1of1fire@gmail.com>
2024-04-02 22:08:43 +0300

    feat(DoublePass): added new method allShortsWordsDP() -> his method finds the shortest words and outputs them in string for double cycle pass

--- src/DoublePass.java
+++ src/DoublePass.java
@@ -2,15 +2,19 @@ import java.util.*;
 /*
 Double Pass Algorithms
     1. allShortsWords() - This method finds the shortest words and outputs them in string
-
+    2. allShortsWordsDP() - This method finds the shortest words and outputs them in string for double cycle pass
 */
 
 public class DoublePass {
     public static void main(String[] args) {
         String testString1 = "This method finds the shortest words and outputs them int string";
+        //one cycle pass
         System.out.println(allShortsWords(testString1));
+        //two cycles pass
+        System.out.println(allShortsWordsDP(testString1));
     }
 
+    // One cycle pass
     public static String allShortsWords(String string) {
         StringBuilder res = new StringBuilder();
         String[] words = string.split(" ");
@@ -29,4 +33,25 @@ public class DoublePass {
         }
         return res.toString();
     }
+
+    // Two cycles pass
+    public static String allShortsWordsDP(String string) {
+        StringBuilder res = new StringBuilder();
+        String[] words = string.split(" ");
+        int shortestLength = words[0].length();
+
+        for (String word : words) {
+            if (word.length() < shortestLength) {
+                shortestLength = word.length();
+            }
+        }
+
+        for (String word : words) {
+            if (word.length() == shortestLength) {
+                res.append(word).append(" ");
+            }
+        }
+        return res.toString();
+    }
+
 }
