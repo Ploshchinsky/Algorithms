983ff18c6db9ecaa6bcbe730485345cfdfa2782a
Anton P. <made1of1fire@gmail.com>
2024-04-09 12:00:22 +0300

    feat(main): added method getAllZeroValue() -> returns number of intervals with zero sum

--- src/PrefixSum.java
+++ src/PrefixSum.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
  *
@@ -7,6 +9,12 @@ import java.util.Arrays;
  *
  * To find the sum of the array with intervals from left to right,
  * it is necessary to call the getSum(left, right) method
+ *
+ * 1.prefixSumCalculate() - counts the prefix sum
+ * 2.prefixSumZeroCalculate() - counts the prefix sum for incoming zeros
+ * 3.getSum() - returns the sum of elements in the given interval
+ * 4.getSumZero() - returns the amount of zeros in the array
+ * 5.getAllZeroValue() - returns number of intervals with zero sum
  *
  * */
 public class PrefixSum {
@@ -53,10 +61,29 @@ public class PrefixSum {
         return prefixSum;
     }
 
+    public int getAllZeroValue() {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : prefixSum) {
+            if (!map.containsKey(num)) {
+                map.put(num, 0);
+            }
+            map.put(num, map.get(num) + 1);
+        }
+        int zeroCounter = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) >= 2) {
+                zeroCounter += map.get(key) - 1;
+            }
+        }
+        return zeroCounter;
+    }
+
 
     public static void main(String[] args) {
 //        int[] input = {5, 3, 8, 1, 4, 6, 13, 1, 7};
-        int[] input = {0, 2, 0, 0, 4, 3, 0};
+        int[] input = {2, 5, -5, 0, 1, 1, -2, 9, 0, 0, 4, 8, -8};
+//        int[] input = {0, 2, 0, 0, 4, 3, 0};
         int left = 2;
         int right = input.length;
         PrefixSum prefixSum = new PrefixSum(input);
@@ -65,6 +92,7 @@ public class PrefixSum {
         System.out.println(Arrays.toString(prefixSum.getSum()));
 
         System.out.printf("\nPrefix sum in interval [%d,%d) - %d", left, right, prefixSum.getSum(left, right));
-        System.out.printf("\nPrefix sum zeros in interval [%d,%d) - %d", left, right, prefixSum.getSumZero(left, right));
+        System.out.printf("\nZeros in interval [%d,%d) - %d", left, right, prefixSum.getSumZero(left, right));
+        System.out.printf("\nNumber of intervals with zero sum - %d", prefixSum.getAllZeroValue());
     }
 }
