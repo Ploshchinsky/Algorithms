8f5a9b3a6704170aa94ce6fe4b07b051cde090d9
Anton P. <made1of1fire@gmail.com>
2024-09-30 00:04:17 +0500

    fix(LeetCode): RLE success -> correct result (include last symbol!)

--- src/RLE.java
+++ src/RLE.java
@@ -8,57 +8,32 @@ import java.util.Map;
  * */
 public class RLE {
     public static void main(String[] args) {
-        String test = "AAAABBCDDDDDEE";
+        String test = "AAAABBCDDDDDEEF";
 
         //Single Pass based algorithm
         System.out.println(RLE(test));
-        //Map based algorithm
-        System.out.println(RLEMap(test));
+
     }
 
     public static String RLE(String string) {
         StringBuilder builder = new StringBuilder();
-        char[] chars = string.toCharArray();
 
-        char currentChar = chars[0];
-        int currentSize = 0;
+        int l = 0;
+        int r = 0;
 
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] == currentChar) {
-                currentSize++;
-                if (i == chars.length - 1) {
-                    builder.append(currentChar).append(currentSize > 1 ? currentSize : "");
-                }
+        while (r < string.length()) {
+            if (string.charAt(l) == string.charAt(r)) {
+                r++;
             } else {
-                builder.append(currentChar).append(currentSize > 1 ? currentSize : "");
-                currentSize = 1;
-                currentChar = chars[i];
-
+                builder.append(string.charAt(l)).append(r - l > 1 ? r - l : "");
+                l = r;
+            }
+            if (r == string.length()){
+                builder.append(string.charAt(l)).append(r - l > 1 ? r - l : "");
             }
         }
         return builder.toString();
     }
 
-    public static String RLEMap(String string) {
-        StringBuilder res = new StringBuilder();
-        Map<Character, Integer> charsMap = new HashMap<>();
-        char[] chars = string.toCharArray();
-
-        charsMap.put(chars[0], 1);
-
-        for (int i = 1; i < chars.length; i++) {
-            if (!charsMap.containsKey(chars[i])) {
-                res.append(chars[i - 1]).append(charsMap.get(chars[i - 1]) > 1 ? charsMap.get(chars[i - 1]) : "");
-                charsMap.put(chars[i], 1);
-            } else {
-                charsMap.put(chars[i], charsMap.get(chars[i]) + 1);
-                if (i == chars.length - 1) {
-                    res.append(chars[i]).append(charsMap.get(chars[i]) > 1 ? charsMap.get(chars[i]) : "");
-                }
-            }
-        }
-
-        return res.toString();
-    }
 
 }
